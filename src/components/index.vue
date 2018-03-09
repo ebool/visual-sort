81137d4a05ca66ab1d6606a040d4ec689591af5d
simon <simon@Parksimonui-MacBook-Pro.local>
2018-03-09 17:56:33 +0900

    adapt vuetify

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,38 +1,31 @@
 <template>
-  <div class="button-container">
-    <router-link to="bubble">거품정렬</router-link>
-    <router-link to="selection">선택정렬</router-link>
-    <router-link to="insertion">삽입정렬</router-link>
-  </div>
+  <v-app dark class="v-app">
+    <action-bar></action-bar>
+    <navigation-drawer></navigation-drawer>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import ActionBar from 'commons/ActionBar.vue';
+import NavigationDrawer from 'commons/NavigationDrawer.vue';
+
 export default {
+  components: {
+    ActionBar,
+    NavigationDrawer
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.button-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-
-  .box {
-    width: 400px;
-    height: 200px;
-    border: 1px solid red;
-    overflow: scroll;
-
-    .inner-box {
-      background-color: greenyellow;
-      width: 200px;
-      height: 900px;
-    }
-  }
-
-  a {
-    margin-top: 10px;
-  }
-}
+@import '~node/vuetify/dist/vuetify.min.css';
 </style>
