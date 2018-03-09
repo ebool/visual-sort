5c07d31c322414f6f86efe46087e1f4a5d75dbb2
simon <simon@Parksimonui-MacBook-Pro.local>
2018-03-09 18:05:26 +0900

    merge

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="button-container">
-    <router-link to="bubble">거품정렬</router-link>
-    <router-link to="selection">선택정렬</router-link>
-    <router-link to="insertion">삽입정렬</router-link>
-  </div>
-</template>
-
-<script>
-export default {
-=======
   <v-app dark class="v-app">
     <action-bar></action-bar>
     <navigation-drawer></navigation-drawer>
@@ -34,36 +23,9 @@ export default {
     ActionBar,
     NavigationDrawer
   }
->>>>>>> adapt-vutify
 }
 </script>
 
 <style lang="scss" scoped>
-<<<<<<< HEAD
-.button-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-
-  .box {
-    width: 400px;
-    height: 200px;
-    border: 1px solid red;
-    overflow: scroll;
-
-    .inner-box {
-      background-color: greenyellow;
-      width: 200px;
-      height: 900px;
-    }
-  }
-
-  a {
-    margin-top: 10px;
-  }
-}
-=======
 @import '~node/vuetify/dist/vuetify.min.css';
->>>>>>> adapt-vutify
 </style>
